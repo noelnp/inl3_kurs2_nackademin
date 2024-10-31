f9993c6c5c63b79738805f0b698a29cd92561499
Rafi Bayat <rafibayat01@outlook.com>
2024-10-31 14:03:47 +0100

    Börjar med GUI för GameApp

--- src/GameApp.java
+++ src/GameApp.java
@@ -1,2 +1,25 @@
-public class GameApp {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameApp extends JFrame implements ActionListener {
+
+    private JButton[][] buttons = new JButton[4][4];
+    private JPanel boardPanel;
+
+
+    public GameApp() {
+        setTitle("15-Spel");
+        setSize(400,450);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new FlowLayout());
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
