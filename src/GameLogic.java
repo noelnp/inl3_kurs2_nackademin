b23fa6275db88f4b00c351efd7fcac183c7d3735
noelnp <noel.peltomaki@gmail.com>
2024-10-31 13:11:16 +0100

    Lägg till metoder i GameLogic

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class GameLogic {
+    private int[][] board;
+
+    public GameLogic() {
+        initBoard();
+    }
+
+    private void initBoard() {
+        // Initiera spelet med nummer och blanda dem
+    }
+
+    public boolean canMoveBrick(int brick) {
+        // Kolla om brickan kan flyttas på
+        return true;
+    }
+
+    public void moveBrick(int brick) {
+        // Flytta brickan om det är möjligt
+    }
+
+    public boolean isSolved() {
+        // Kolla om spelet är avklarat
+        return true;
+    }
 }
