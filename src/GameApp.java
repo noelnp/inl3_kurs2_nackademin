9ba20617f8998754703bef3d5579166bbdc75dc3
Rafi Bayat <rafibayat01@outlook.com>
2024-11-01 12:40:18 +0100

    Lägger till initializeBoard i GUI

--- src/GameApp.java
+++ src/GameApp.java
@@ -31,6 +31,24 @@ public class GameApp extends JFrame implements ActionListener {
         add(boardPanel,BorderLayout.CENTER);
     }
 
+    private void initializeBoard(){
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 4; col++) {
+                buttons[row][col] = new JButton();
+                buttons[row][col].setFont(new Font("Arial", Font.BOLD, 20));
+                buttons[row][col].addActionListener(this);
+                boardPanel.add(buttons[row][col]);
+            }
+        }
+    }
+
+
+
+
+
+
+    private void updateBoard(){}
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
