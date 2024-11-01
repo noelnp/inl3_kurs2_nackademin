0c403564633059269e5726c4c157a1a71d043577
Rafi Bayat <rafibayat01@outlook.com>
2024-11-01 11:47:52 +0100

    Uppdaterar GUI och lägger till konstruktor

--- src/GameApp.java
+++ src/GameApp.java
@@ -6,17 +6,29 @@ import java.awt.event.ActionListener;
 public class GameApp extends JFrame implements ActionListener {
 
     private JButton[][] buttons = new JButton[4][4];
+   // private GameLogic gameLogic;
     private JPanel boardPanel;
 
 
     public GameApp() {
+        // gameLogic = new GameLogic();
+        setupFrame();
+        initializeBoard();
+        updateBoard();
+        setVisible(true);
+
+
+    }
+
+    private void setupFrame(){
         setTitle("15-Spel");
         setSize(400,450);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setLayout(new FlowLayout());
-        setVisible(true);
 
+        //Skapar panelen med GridLayout
+        boardPanel = new JPanel(new GridLayout(4,4));
+        add(boardPanel,BorderLayout.CENTER);
     }
 
 
